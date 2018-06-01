d39aa9d92361edc95ce7f5155df24ddc869a2435
ricky <rickyputra98@gmail.com>
2018-06-01 19:44:40 +0700

    add to indonesia and text to speech

--- sample.py
+++ sample.py
@@ -1,14 +1,20 @@
 import torch
 import matplotlib.pyplot as plt
-import numpy as np 
+import numpy as np
 import argparse
-import pickle 
+import pickle
 import os
-from torch.autograd import Variable 
-from torchvision import transforms 
+from torch.autograd import Variable
+from torchvision import transforms
 from build_vocab import Vocabulary
 from model import EncoderCNN, DecoderRNN
 from PIL import Image
+from googletrans import Translator
+from gtts import gTTS
+from io import BytesIO
+from playsound import playsound
+
+mp3_fp = BytesIO()
 
 
 def to_var(x, volatile=False):
@@ -19,19 +25,19 @@ def to_var(x, volatile=False):
 def load_image(image_path, transform=None):
     image = Image.open(image_path)
     image = image.resize([224, 224], Image.LANCZOS)
-    
+
     if transform is not None:
         image = transform(image).unsqueeze(0)
-    
+
     return image
-    
+
 def main(args):
     # Image preprocessing
     transform = transforms.Compose([
-        transforms.ToTensor(), 
-        transforms.Normalize((0.485, 0.456, 0.406), 
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406),
                              (0.229, 0.224, 0.225))])
-    
+
     # Load vocabulary wrapper
     with open(args.vocab_path, 'rb') as f:
         vocab = pickle.load(f)
@@ -39,9 +45,9 @@ def main(args):
     # Build Models
     encoder = EncoderCNN(args.embed_size)
     encoder.eval()  # evaluation mode (BN uses moving mean/variance)
-    decoder = DecoderRNN(args.embed_size, args.hidden_size, 
+    decoder = DecoderRNN(args.embed_size, args.hidden_size,
                          len(vocab), args.num_layers)
-    
+
 
     # Load the trained model parameters
     encoder.load_state_dict(torch.load(args.encoder_path))
@@ -50,17 +56,17 @@ def main(args):
     # Prepare Image
     image = load_image(args.image, transform)
     image_tensor = to_var(image, volatile=True)
-    
+
     # If use gpu
     if torch.cuda.is_available():
         encoder.cuda()
         decoder.cuda()
-    
+
     # Generate caption from image
     feature = encoder(image_tensor)
     sampled_ids = decoder.sample(feature)
     sampled_ids = sampled_ids.cpu().data.numpy()
-    
+
     # Decode word_ids to words
     sampled_caption = []
     for word_id in sampled_ids:
@@ -69,12 +75,21 @@ def main(args):
         if word == '<end>':
             break
     sentence = ' '.join(sampled_caption)
-    
+
     # Print out image and generated caption.
-    print ('This is an image of: ' + sentence.replace('<start> ', '').replace(' <end>', ''))
+    sentence = sentence.replace('<start> ', '').replace(' <end>', '').replace('.', '').strip()
+    translator = Translator()
+    sentence_indo = translator.translate(sentence, dest='id').text
+    print('This is an image of: ' + sentence_indo)
+    tts = gTTS(sentence_indo, 'id')
+    tts.save('result.mp3')
+    playsound('result.mp3')
+
     image = Image.open(args.image)
     plt.imshow(np.asarray(image))
     plt.show()
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--image', type=str, required=True,
@@ -85,7 +100,7 @@ if __name__ == '__main__':
                         help='path for trained decoder')
     parser.add_argument('--vocab_path', type=str, default='./data/vocab.pkl',
                         help='path for vocabulary wrapper')
-    
+
     # Model parameters (should be same as paramters in train.py)
     parser.add_argument('--embed_size', type=int , default=256,
                         help='dimension of word embedding vectors')
